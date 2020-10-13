7c569e8107ac11e2a38bcf86a9f3ea556b41f969
DimaHaletski <dimahaletski@gmail.com>
2020-10-13 10:19:17 +0300

    Dom parser first version

---
+++ src/Runner.java
@@ -0,0 +1,9 @@
+import by.gsu.pms.ParserDOM;
+
+import javax.swing.text.html.parser.Parser;
+
+public class Runner {
+    public static void main(String[] args) {
+        ParserDOM.parse("src/bank.xml");
+    }
+}
